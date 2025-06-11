a1b001a9a9cad858f782886982dd097294ee2cb1
Coronel <160642659+helio000@users.noreply.github.com>
2025-06-11 13:56:21 -0300

    Update login.js

--- api/src/controllers/login.js
+++ api/src/controllers/login.js
@@ -1,8 +1,10 @@
+const { gerarToken } = require('../utils/token');
+
 exports.login = (req, res) => {
   const { email, senha } = req.body;
-
   if (email === 'usuario@gmail.com' && senha === '123456') {
-    return res.json({ token: 'seu-token-aqui' });
+    const token = gerarToken({ email });
+    return res.json({ token });
   }
   return res.status(401).json({ erro: 'Login inválido' });
 };
